107982d96504a1ac8c550e8631120dec3dcab580
abhishek kumar <abhisingh2084@gmail.com>
2024-05-23 18:40:28 +0530

    Add project files.

---
+++ ModelBindingASPCore/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBindingASPCore.Models;
+using System.Diagnostics;
+
+namespace ModelBindingASPCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Details(int id, string name)
+        {
+            return "id is : " + id + " Name is : " + name;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
